f08d5b450b6ba17e8c64678e62af626a9c238b92
QLV5645-QLJ <qinjielin2018@u.northwestern.edu>
2020-04-22 20:05:25 -0500

    construct motor position

--- bipedal_debug.py
+++ bipedal_debug.py
@@ -8,12 +8,12 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())  # to load plane.urdf
 p.loadURDF("plane.urdf")
 # p.loadURDF("urdf/plane.urdf",basePosition=[0,0,-1.5])
 # humanoid = p.loadURDF("cassie/urdf/cassie_collide.urdf",[0,0,0.8], useFixedBase=False)
-# humanoid = p.loadURDF("urdf/simbicon_urdf/biped2d.urdf")
-humanoid = p.loadURDF("urdf/simbicon_urdf/humanoid_nohead.urdf",[0, 0, 0.31])
-# humanoid = p.loadURDF("urdf/simbicon_urdf/flame.urdf")
+# humanoid = p.loadURDF("urdf/simbicon_urdf/biped2d.urdf",[0, 0, 1.2])
+# humanoid = p.loadURDF("urdf/simbicon_urdf/humanoid_nohead.urdf",[0, 0, 0.31])
+humanoid = p.loadURDF("urdf/simbicon_urdf/flame.urdf",[0, 0, 0.7])
 # humanoid = p.loadURDF("urdf/simbicon_urdf/demo.urdf")
 # gravId = p.addUserDebugParameter("gravity",-10,10,-10)
-gravId = p.addUserDebugParameter("gravity",-10,10,-10)
+gravId = p.addUserDebugParameter("gravity",-10,10,0)
 jointIds=[]
 paramIds=[]
 
@@ -30,8 +30,8 @@ for j in range (p.getNumJoints(humanoid)):
     jointType = info[2]
     if (jointType==p.JOINT_PRISMATIC or jointType==p.JOINT_REVOLUTE):
         activeJoint+=1
-        if (activeJoint == 1 or activeJoint == 6 or activeJoint == 7 or activeJoint == 12):
-            continue
+        # if (activeJoint == 1 or activeJoint == 6 or activeJoint == 7 or activeJoint == 12):
+        #     continue
         jointIds.append(j)
         paramIds.append(p.addUserDebugParameter(jointName.decode("utf-8"),-4,4,jointAngles[activeJoint]))
         # p.resetJointState(humanoid, j, jointAngles[activeJoint])
