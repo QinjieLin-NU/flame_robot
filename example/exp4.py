575eb2b62f8bca413411680d13dd0bebd2405506
qinjielin-nu <qinjielin2018@u.northwestern.edu>
2021-08-29 02:45:24 +0000

    add 3d robot trajectory with ankle torque

---
+++ example/exp4.py
@@ -0,0 +1,52 @@
+import envs
+from envs.bipedal_bullet_env_rl_flame8_a6 import BipedalBulletRLEnvFLAME8A6 
+from envs.bipedal_bullet_env_rl_flame10_a6 import BipedalBulletRLEnvFLAME10A6 
+from ray import tune
+from ray.rllib.agents.ppo import PPOTrainer
+from ray.tune.registry import register_env
+import ray
+import ray.rllib.agents.ppo as ppo
+from ray.tune.logger import pretty_print
+import gym
+import time
+
+env_name = 'BipedalPyBulletEnv-v11'
+# instantiate env class
+def env_creator(env_config):
+    import envs
+    return gym.make(env_name)  # return an env instance
+register_env("my_env", env_creator)
+
+#trainer config
+ray.init()
+config = ppo.DEFAULT_CONFIG.copy()
+config["num_gpus"] = 0
+config["num_workers"] = 1
+config["explore"] = False
+config["framework"] = "torch"
+config["seed"] = 12345
+config["num_sgd_iter"] = 20
+config["kl_coeff"]=1.0
+config["lr"] = 0.0001
+config["sgd_minibatch_size"] =2560#32768
+config["train_batch_size"] = 25600#320000
+config["batch_mode"] = "complete_episodes"
+config["observation_filter"]  = "MeanStdFilter"
+trainer = ppo.PPOTrainer(config=config, env="my_env")
+restore_path = "/root/ray_results/PPO_my_env_2021-08-23_22-00-43bnev_f58/checkpoint_009886/checkpoint-9886"
+trainer.restore(restore_path) 
+
+# run until episode ends
+env = gym.make(env_name)
+for i in range(100):
+    episode_reward = 0
+    done = False
+    env.render()
+    obs = env.reset()
+    while not done:
+        action = trainer.compute_action(obs)
+        # print(action)
+        obs, reward, done, info = env.step(action)
+        episode_reward += reward
+        time.sleep(0.1)
+    print("episode reward:",episode_reward)
--- example/load_data.py
+++ example/load_data.py
@@ -1,27 +1,27 @@
 import pickle
 
-def get_thetas():
+def get_thetas(file_name):
     """
     return a theta array[1000,7]: theta of centerHip,RHip,RKnee,RAnkleY,LHip,LKnee,LAnkleY
     """
     data_dict = None
-    with open('example/data/exp2_trajectory.pkl', 'rb') as f:
+    with open(file_name, 'rb') as f:
         data_dict = pickle.load(f)
     state = data_dict["state_list"] #including vel, theta and collsision
     motor_thetas = state[:,8:22:2]
     return motor_thetas
 
-def get_torques():
+def get_torques(file_name):
     """
     return a torque array[1000,7]: torque of centerHip,RHip,RKnee,RAnkleY,LHip,LKnee,LAnkleY
     """
     data_dict = None
-    with open('example/data/exp2_trajectory.pkl', 'rb') as f:
+    with open(file_name, 'rb') as f:
         data_dict = pickle.load(f)
     action = data_dict["action_list"] #toque of centerHip,RHip,RKnee,RAnkleY,LHip,LKnee,LAnkleY
     return action
 
-theta_list = get_thetas()
-toruqe_list = get_torques()
+theta_list = get_thetas(file_name='example/data/exp4_trajectory.pkl')
+toruqe_list = get_torques(file_name='example/data/exp4_trajectory.pkl')
 print(theta_list.shape,toruqe_list.shape)
 print(theta_list)
